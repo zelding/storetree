01c779663206d4d6bf19e0128684f8c9faf431c6
zedling <gammaray.zedling@gmail.com>
2017-02-17 11:22:08 +0100

    - added datatables css - modified authenticated redirect route

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ elixir(function(mix) {
     mix.copy(paths.select2 + "dist/js/select2.full.js", "resources/assets/js")
         .copy(paths.select2 + "dist/css/select2.css", "resources/assets/css")
         .copy(paths.select2bs + "dist/select2-bootstrap.css", "resources/assets/css")
+        .copy('node_modules/datatables/media/css/jquery.dataTables.css', 'resources/assets/css')
         .copy('node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.less', 'resources/assets/less');
 
     mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
@@ -18,6 +19,7 @@ elixir(function(mix) {
         .styles([
             'main.css',
             'select2.css',
+            'jquery.dataTables.css',
             'select2-bootstrap.css'
         ], 'public/css/main.css')
         .browserify([
