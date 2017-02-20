f086ae1ea24dd4c92043bbffb8cdd93b5e5b5cd7
zedling <gammaray.zedling@gmail.com>
2017-02-20 18:35:13 +0100

    - started txt migrations

---
+++ app/Constants.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * storetree
+ *
+ * @author    lyozsi (kristof.dekany@apex-it-services.eu)
+ * @copyright internal usage
+ *
+ * Date: 2/20/17 6:09 PM
+ */
+
+namespace App;
+
+final class Constants
+{
+    public static $shareable = ['ITEM_NOT_SHAREABLE', 'ITEM_PARTIALLY_SHAREABLE', 'ITEM_FULLY_SHAREABLE', 'ITEM_FULLY_SHAREABLE_STACKING'];
+
+    public static $itemQuality = ['consumable', 'component', 'common', 'rare', 'epic', 'artifact'];
+
+    public static $disassemble = ['DOTA_ITEM_DISASSEMBLE_NEVER', 'DOTA_ITEM_DISASSEMBLE_ALWAYS'];
+
+    public static $targetType = ['DOTA_UNIT_TARGET_HERO', 'DOTA_UNIT_TARGET_BASIC'];
+
+    public static $declaration = ['DECLARE_PURCHASES_TO_TEAMMATES', 'DECLARE_PURCHASES_IN_SPEECH', 'DECLARE_PURCHASES_TO_SPECTATORS'];
+
+    public static $varTypes = ['FIELD_INTEGER', 'FIELD_FLOAT'];
+}
--- database/migrations/2017_02_12_093912_create_items_table.php
+++ database/migrations/2017_02_12_093912_create_items_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Constants;
 
 class CreateItemsTable extends Migration
 {
@@ -15,16 +16,39 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->unsignedInteger('cost')->default(0);
-            $table->unsignedTinyInteger('stack_size')->default(1);
+            $table->integer('dota_id')->unique()->nullable()->default(null);
+            $table->string('base_class')->unique()->nullable()->default(null);
+            $table->tinyInteger('max_level')->default(1);
+            $table->tinyInteger('base_level')->default(1);
             $table->boolean('is_consumable')->default(false);
             $table->boolean('is_base_item')->default(false);
             $table->boolean('is_boss_item')->default(false);
             $table->boolean('is_recipe')->default(false);
+            $table->boolean('is_killable')->default(true);
+            $table->boolean('is_sellable')->default(true);
+            $table->boolean('is_purchasable')->default(true);
+            $table->boolean('is_droppable')->default(true);
+            $table->boolean('in_backpack')->default(true);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedInteger('cost')->default(0);
+            $table->unsignedTinyInteger('stack_size')->default(1);
+            $table->string('model')->nullable()->default('models/props_gameplay/recipe.vmdl');
+            $table->tinyInteger('fight_recap')->default(0);
+            $table->enum('quality', Constants::$itemQuality)->default('component');
+            $table->enum('share', Constants::$shareable)->default('ITEM_NOT_SHAREABLE');
+            $table->tinyInteger('stock_max')->default(0);
+            $table->mediumInteger('stock_time')->default(0);
+            $table->tinyInteger('stock_initial')->default(0);
+            $table->tinyInteger('start_charges')->default(0);
+            $table->boolean('show_charges')->default(false);;
+            $table->boolean('needs_charges')->default(false);
+            $table->boolean('is_autocast')->default(false);
+            $table->boolean('is_alertable')->default(false);
+            $table->string('alert_text')->nullable();
+            //shopTags
+            //Abilities
             $table->timestamps();
         });
     }
