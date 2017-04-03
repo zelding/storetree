1b4a28854e369477db221e0997fe69fb557af150
zedling <gammaray.zedling@gmail.com>
2017-04-03 23:47:17 +0200

    - mysql update on travis again 9

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
 
@@ -14,6 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Validator::extend('denied_with', function ($attribute, $value, $parameters, $validator) {
             /**@var \Illuminate\Validation\Validator $validator */
 
