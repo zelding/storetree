a40fc51cd1161d231faf2ffba83e356c2bb92bac
zedling <gammaray.zedling@gmail.com>
2017-04-04 08:39:53 +0200

    - corrected ability base_class definition

--- database/migrations/2017_03_03_212722_create_abilities_table.php
+++ database/migrations/2017_03_03_212722_create_abilities_table.php
@@ -17,7 +17,7 @@ class CreateAbilitiesTable extends Migration
             $table->increments('id');
             $table->string('name')->unique();
 
-            $table->string('base_class')->unique();
+            $table->string('base_class')->unique()->nullable();
             $table->text('behaviour');
             $table->enum('type', \App\Utils\Constants::$abilityType)->default('DOTA_ABILITY_TYPE_BASIC');
             $table->string('texture_name')->nullable();
