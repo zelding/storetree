8630d289908a6375b53605f3e674d906997f0734
kdekany <kristof.dekany@apex-it-services.eu>
2018-01-22 13:32:21 +0100

    - updated uft16 headers (tooltip preview)

--- app/Http/Middleware/Utf16.php
+++ app/Http/Middleware/Utf16.php
@@ -15,14 +15,31 @@ use Illuminate\Http\Response;
 
 class Utf16
 {
+    /**
+     * @param         $request
+     * @param Closure $next
+     *
+     * @link https://laracasts.com/discuss/channels/general-discussion/add-content-length-header-on-views
+     *
+     * @return Response
+     */
     public function handle($request, Closure $next)
     {
         /** @var Response $response */
         $response    = $next($request);
-        $content     = iconv("utf-8", 'utf-16be', $response->getContent());
+
+        // to be sure nothing was already output (by an echo statement or something)
+        if (headers_sent() || ob_get_contents() != '') return $response;
+
+        //$content     = iconv("utf-8", 'utf-16be', $response->getContent()); // - not sure if needed
+        $content     = $response->getContent();
+
+        $contentLength = mb_strlen($content) * 16;
 
         $response->header('Content-Type', 'text/html charset=utf-16be')
                  ->header('Content-Disposition', 'inline')
+                 ->header("Connection", "Close")
+                 ->header("Content-Length",  $contentLength)
                  ->setContent( $content );
 
         return $response;
