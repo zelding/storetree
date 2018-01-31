9b941390570fc04572564f5b076d27f963369408
kdekany <kristof.dekany@apex-it-services.eu>
2018-01-31 15:46:18 +0100

    - added sortable to item_stat edit - no effect yet

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,20 @@ elixir(function(mix) {
        .copy(paths.select2 + "dist/css/select2.css", paths.css_prv)
        .copy(paths.select2bs + "dist/select2-bootstrap.css", paths.css_prv)
        .copy('node_modules/datatables/media/css/jquery.dataTables.css', paths.css_prv)
+
+
+       //.copy('node_modules/jquery-sortable/source/css/jquery-sortable.css.sass', 'resources/assets/sass')
+
        .copy('node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.less', 'resources/assets/less')
        .copy('bower_components/iCheck/icheck.js', 'public/js')
        .copy('bower_components/iCheck/skins/minimal/_all.css', 'public/css')
+       .copy('node_modules/jquery-sortable/source/js/jquery-sortable-min.js', 'public/js')
     ;
 
     mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
     mix.copy('bower_components/iCheck/skins/minimal/**.png', 'public/css');
+
+    //mix.sass('jquery-sortable.css.sass', 'public/css');
 
     mix.sass('app.scss')
         .less('datatables-bootstrap3.less')
